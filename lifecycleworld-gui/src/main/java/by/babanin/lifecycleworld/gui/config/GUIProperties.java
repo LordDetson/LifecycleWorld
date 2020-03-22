3d99df7028a601e495ec191f27a96d89349c0439
Babanin Dmitry <babanin.dima@gmail.com>
2020-03-22 23:31:03 +0300

    feat: added configuration data to get an icon for the settings button

--- lifecycleworld-gui/src/main/java/by/babanin/lifecycleworld/gui/config/GUIProperties.java
+++ lifecycleworld-gui/src/main/java/by/babanin/lifecycleworld/gui/config/GUIProperties.java
@@ -1,12 +1,15 @@
 package by.babanin.lifecycleworld.gui.config;
 
 public class GUIProperties {
+    public static final String SETTINGS_ICON_PATH = "settings.icon.path";
+
     private static final String JAVA_AWT_HEADLESS = "java.awt.headless";
+    private static final String DEFAULT_SETTINGS_ICON_PATH = "img/icons/settings.png";
 
     private GUIProperties(){}
 
     public static void setUpProperties() {
         System.setProperty(JAVA_AWT_HEADLESS, Boolean.FALSE.toString());
+        System.setProperty(SETTINGS_ICON_PATH, DEFAULT_SETTINGS_ICON_PATH);
     }
-
 }
